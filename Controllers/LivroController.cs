8982dc59a756bcb708a4a28a27ab46e64c259b7a
unknown <Vitor>
2021-08-09 10:09:12 -0300

    Alteração no LivroController

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -1,5 +1,9 @@
 using Biblioteca.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 
 namespace Biblioteca.Controllers
 {
@@ -49,5 +53,14 @@ namespace Biblioteca.Controllers
             Livro l = ls.ObterPorId(id);
             return View(l);
         }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(string.IsNullOrEmpty(HttpContext.Session.GetString("user")))
+            {
+                filterContext.HttpContext.Response.Redirect("/Home/Login");
+            }
+        }
+
     }
 }
